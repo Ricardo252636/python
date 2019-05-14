d553da50cabaa4cd297dab41e1b95d4286cb3361
Ricardo252636 <kdc252636@gmail.com>
2019-05-13 21:58:12 -0500

    Algunos archivos de python

---
+++ def.py
@@ -0,0 +1,19 @@
+"""def test () :
+	print("test ejecutada")
+
+print("Podriamos hacer muchas lineas de codigo")
+print("para despues llamar la definicion")
+test()"""
+
+def test2 (a, b): 
+	a = 2
+	b = 3
+
+c = 5
+d = 6
+e = 1
+f = 2
+
+test2 (e, d)
+print("c={0}, d={1}".format(e ,c))
+
---
+++ for.py
@@ -0,0 +1,13 @@
+"""for x in range (1, 4):
+	print(x)"""
+
+"""lista = ["uno", "dos", "tres"]
+cad = ""
+for ele in lista:
+	cad += ele
+print(cad)"""
+
+"""for item in (1, 2, 3):
+	print(item)
+	else:#esto solo es valido para python 3
+		print("fin")"""
